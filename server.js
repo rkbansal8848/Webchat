dd146b9fcfde970bb3cb156a2813b210ff37259f
RK Bansal <rkbansal8848@gmail.com>
2023-05-27 00:25:25 +0530

    Server Modifications

--- server.js
+++ server.js
@@ -5,21 +5,26 @@ const io = require('socket.io')(process.env.PORT || 8000, {
     methods: ['GET', 'POST']
   }
 });
-
 const users = {};
 
 io.on('connection', socket => {
-  socket.on('new-user-joined', name => {
-    users[socket.id] = name;
-    socket.broadcast.emit('user-joined', name);
+  socket.on('new-user-joined', ({ name, code }) => {
+    // Assign user to the specified group based on the code
+    socket.join(code);
+    users[socket.id] = { name, code };
+    socket.broadcast.to(code).emit('user-joined', name);
   });
 
-  socket.on('send', message => {
-    socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+  socket.on('send', ({ message, code }) => {
+    // Send the message to all users in the same group as the sender
+    socket.broadcast.to(code).emit('receive', { message, name: users[socket.id].name });
   });
 
-  socket.on('disconnect', message => {
-    socket.broadcast.emit('left', users[socket.id]);
-    delete users[socket.id];
+  socket.on('disconnect', () => {
+    if (users[socket.id]) {
+      const { name, code } = users[socket.id];
+      socket.broadcast.to(code).emit('left', name);
+      delete users[socket.id];
+    }
   });
 });
